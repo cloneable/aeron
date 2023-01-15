c39d670bbaa4de01d6a1a9ccfe015bf9ae8e2381
Folke Behrens <folke@gmail.com>
2023-01-15 22:27:17 +0100

    Use channel and stream id from aeron examples

--- examples/src/bin/async.rs
+++ examples/src/bin/async.rs
@@ -3,13 +3,16 @@ use aeron::{
     context::Context,
 };
 
+const DEFAULT_CHANNEL: &str = "aeron:udp?endpoint=localhost:20121";
+const DEFAULT_STREAM_ID: StreamId = StreamId(1001);
+
 #[tokio::main]
 pub async fn main() -> color_eyre::Result<()> {
     let ctx = Context::new()?;
     let client = Aeron::connect(ctx)?;
 
-    let mut _publication = client
-        .add_publication(&"aeron:ipc".to_owned(), StreamId(1001))?
+    let mut publication = client
+        .add_publication(&DEFAULT_CHANNEL.to_owned(), DEFAULT_STREAM_ID)?
         .await?;
 
     println!("Yay!");
