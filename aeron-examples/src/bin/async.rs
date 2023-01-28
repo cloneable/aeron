a08c4a99e7e8347ee4b0cc9bb2d6e505602a6bf9
Folke Behrens <folke@gmail.com>
2023-01-28 09:34:03 +0100

    Create two separate clients for pub/sub

--- aeron-examples/src/bin/async.rs
+++ aeron-examples/src/bin/async.rs
@@ -10,36 +10,40 @@ const DEFAULT_STREAM_ID: StreamId = StreamId(1001);
 #[tokio::main]
 pub async fn main() -> color_eyre::Result<()> {
     let mut ctx = Context::new()?;
-
     ctx.set_error_handler(|code, msg| {
-        println!("ERR{code}: {msg}");
+        println!("P: ERR{code}: {msg}");
     });
-
     ctx.set_on_new_publication(|channel, stream_id, session_id, correlation_id| {
-        println!("New publication: channel={channel} stream_id={stream_id:?} session_id={session_id:?} correlation_id={correlation_id:?}");
+        println!("P: New publication: channel={channel} stream_id={stream_id:?} session_id={session_id:?} correlation_id={correlation_id:?}");
     });
-
     ctx.set_on_new_subscription(|channel, stream_id, correlation_id| {
-        println!("New subscription: channel={channel} stream_id={stream_id:?} correlation_id={correlation_id:?}");
+        println!("P: New subscription: channel={channel} stream_id={stream_id:?} correlation_id={correlation_id:?}");
     });
+    let client_p = Aeron::connect(ctx)?;
 
-    let client = Aeron::connect(ctx)?;
+    let mut publication = client_p.add_publication(DEFAULT_CHANNEL, DEFAULT_STREAM_ID)?.await?;
 
-    let mut publication =
-        client.add_publication(&DEFAULT_CHANNEL.to_owned(), DEFAULT_STREAM_ID)?.await?;
+    let mut ctx = Context::new()?;
+    ctx.set_error_handler(|code, msg| {
+        println!("S: ERR{code}: {msg}");
+    });
+    ctx.set_on_new_publication(|channel, stream_id, session_id, correlation_id| {
+        println!("S: New publication: channel={channel} stream_id={stream_id:?} session_id={session_id:?} correlation_id={correlation_id:?}");
+    });
+    ctx.set_on_new_subscription(|channel, stream_id, correlation_id| {
+        println!("S: New subscription: channel={channel} stream_id={stream_id:?} correlation_id={correlation_id:?}");
+    });
+    let client_s = Aeron::connect(ctx)?;
 
-    let subscription = client
-        .add_subscription(&DEFAULT_CHANNEL.to_owned(), DEFAULT_STREAM_ID)
-        .unwrap()
-        .await
-        .unwrap();
+    let subscription =
+        client_s.add_subscription(DEFAULT_CHANNEL, DEFAULT_STREAM_ID).unwrap().await.unwrap();
 
     let handle = tokio::spawn(async move {
         let stop = AtomicBool::new(false);
         let handler = |data: &[u8], header: Header| {
             let text = String::from_utf8_lossy(data);
             println!(
-                "Message from session {sess_id:?} ({len} bytes) <<{text}>>",
+                "S: Message from session {sess_id:?} ({len} bytes) <<{text}>>",
                 sess_id = header.session_id(),
                 len = data.len(),
             );
@@ -58,16 +62,16 @@ pub async fn main() -> color_eyre::Result<()> {
     for _ in 0..10 {
         match publication.offer(&buf)? {
             OfferResult::Ok(position) => {
-                println!("SENT {position:?}");
+                println!("P: SENT {position:?}");
             }
             OfferResult::NotConnected => {
-                println!("no subscriber connnected. retying.");
+                println!("P: no subscriber connnected. retying.");
             }
             OfferResult::BackPressured => {
-                println!("back pressured. retying.");
+                println!("P: back pressured. retying.");
             }
             OfferResult::AdminAction => {
-                println!("admin action. retying.");
+                println!("P: admin action. retying.");
             }
         };
         tokio::time::sleep(Duration::from_millis(500)).await;
